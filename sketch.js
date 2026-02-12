c3c569d4f6bb5dee278dd2c2c6d375973e25575f
MilzandMore <Milzandmore@gmail.com>
2026-02-12 10:25:02 +0100

    Erster Upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255, 0, 0); // Der Bildschirm MUSS jetzt knallrot werden
+  fill(255);
+  textSize(32);
+  text("Verbindung steht!", 50, 50);
+}

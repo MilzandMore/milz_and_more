9f1da49cf36ed4333b35d6b5d5faab6870746134
MilzandMore <Milzandmore@gmail.com>
2026-02-14 20:45:03 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,110 @@
 // MASTER SKETCH - Milz & More
 let mainSelect;
 let currentDrawFunction = null;
-let logoImg; // Diese Variable MUSS existieren für die Unterprogramme
+let logoImg;
 let isAdmin = false;
 
 function preload() {
-  // Lädt das Logo zentral, damit mandala_wabe.js etc. es finden
+  // Lädt das Logo für alle Unterprogramme
   logoImg = loadImage('logo.png');
 }
 
 function setup() {
+  // Erstellt die Grundfläche
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100);
   angleMode(RADIANS);
 
-  // Der Umschalter - Wir lassen ihn pur, damit er nicht "klumpig" wirkt
+  // Der Haupt-Umschalter oben links
   mainSelect = createSelect();
-  mainSelect.position(10, 10);
+  mainSelect.position(20, 20); // Etwas mehr Abstand zum Rand
   mainSelect.option('Wabe');
   mainSelect.option('Rund');
   mainSelect.option('Quadrat');
   mainSelect.selected('Wabe'); 
   mainSelect.changed(changeApp);
-  mainSelect.style('z-index', '1000');
 
+  // Styling via JS (zusätzlich zum CSS für garantierte Layering)
+  mainSelect.style('z-index', '1000');
+  
+  // Initialer Start der ersten App
   changeApp();
 }
 
 function draw() {
-  // Wir zeichnen hier KEIN Logo. 
-  // Das macht mandala_wabe.js in Zeile 116 von ganz alleine.
+  background(255);
+  
+  // Falls ein Logo vorhanden ist, zeichnen wir es dezent unten rechts
+  if (logoImg) {
+    push();
+    imageMode(CORNER);
+    let logoW = 100;
+    let ratio = logoImg.height / logoImg.width;
+    image(logoImg, windowWidth - logoW - 20, windowHeight - (logoW * ratio) - 20, logoW, logoW * ratio);
+    pop();
+  }
+
   if (currentDrawFunction) {
     currentDrawFunction();
   }
+}
+
+function changeApp() {
+  hideAllUI();
+  let mode = mainSelect.value();
+
+  if (mode === 'Wabe') {
+    if (typeof setupWabe === "function") {
+      if (typeof topBarWabe === "undefined" || !topBarWabe) setupWabe();
+      showUIWabe();
+      currentDrawFunction = drawWabe;
+    }
+  } 
+  else if (mode === 'Rund') {
+    if (typeof setupRund === "function") {
+      if (typeof topBarRund === "undefined" || !topBarRund) setupRund();
+      showUIRund();
+      currentDrawFunction = drawRund;
+    }
+  } 
+  else if (mode === 'Quadrat') {
+    if (typeof setupQuadrat === "function") {
+      if (typeof topBarQuadrat === "undefined" || !topBarQuadrat) setupQuadrat();
+      showUIQuadrat();
+      currentDrawFunction = drawQuadrat;
+    }
+  }
+}
+
+function hideAllUI() {
+  if (window.topBarWabe) topBarWabe.hide();
+  if (window.sliderPanelWabe) sliderPanelWabe.hide();
+  if (window.topBarRund) topBarRund.hide();
+  if (window.sliderPanelRund) sliderPanelRund.hide();
+  if (window.topBarQuadrat) topBarQuadrat.hide();
+  if (window.sliderPanelQuadrat) sliderPanelQuadrat.hide();
+}
+
+function showUIWabe() {
+  if (window.topBarWabe) topBarWabe.show();
+  if (window.sliderPanelWabe) sliderPanelWabe.show();
+}
+
+function showUIRund() {
+  if (window.topBarRund) topBarRund.show();
+  if (window.sliderPanelRund) sliderPanelRund.show();
+}
+
+function showUIQuadrat() {
+  if (window.topBarQuadrat) topBarQuadrat.show();
+  if (window.sliderPanelQuadrat) sliderPanelQuadrat.show();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  if (typeof updateLayoutWabe === "function") updateLayoutWabe();
+  if (typeof updateLayoutRund === "function") updateLayoutRund();
+  if (typeof updateLayoutQuadrat === "function") updateLayoutQuadrat();
 }
 
 function changeApp() {
@@ -92,3 +164,4 @@ function windowResized() {
   if (typeof updateLayoutRund === "function") updateLayoutRund();
   if (typeof updateLayoutQuadrat === "function") updateLayoutQuadrat();
 }
+

662f69ddd4abd2d0156983c32e8a3b7d6d605a15
MilzandMore <Milzandmore@gmail.com>
2026-02-24 20:31:04 +0100

    Update logo image path in mandala.js

--- engines/rund/mandala.js
+++ engines/rund/mandala.js
@@ -15,7 +15,7 @@ var isAdmin = false;
 
 function preload() {
   // Pfad angepasst für die Ordnerstruktur
-  logoImg = loadImage('../../assets/Logo.png');
+  logoImg = loadImage('/assets/Logo.png');
 }
 
 function setup() {

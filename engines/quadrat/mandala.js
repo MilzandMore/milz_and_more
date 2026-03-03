5a332abd604bda903d93700fd5610e7f9a144da3
MilzandMore <Milzandmore@gmail.com>
2026-03-03 11:47:12 +0100

    Update mandala.js

--- engines/quadrat/mandala.js
+++ engines/quadrat/mandala.js
@@ -198,7 +198,7 @@ async function exportHighRes() {
     const yShift = -200;
 
     for (let x = -100; x < exportW + 400; x += 500) {
-      for (let y = -400; y < exportH + 400; y += 500) {
+      for (let y = -500; y < exportH + 400; y += 500) {
         pg.image(exportLogo, x, y + yShift, wWidth, wHeight);
       }
     }

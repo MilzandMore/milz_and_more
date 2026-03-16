04526773a6dbc44d6f848f51e6388aa91f00f852
MilzandMore <Milzandmore@gmail.com>
2026-03-16 11:17:19 +0100

    Update mandala.js

--- engines/rund/mandala.js
+++ engines/rund/mandala.js
@@ -209,9 +209,9 @@ function drawLiveWatermark() {
   imageMode(CENTER);
 
   drawingContext.save();
-  drawingContext.globalAlpha = 0.32;
+  drawingContext.globalAlpha = 0.28;
 
-  const crownW = min(width, height) * 0.42;
+  const crownW = min(width, height) * 0.38;
   const crownH = (crownImg.height / crownImg.width) * crownW;
 
   image(crownImg, width / 2, height / 2, crownW, crownH);
